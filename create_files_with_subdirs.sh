e1eac190d096d68d8cd8ae19aed8116d2fce8be6
Dean <106890173+Deanosaur666@users.noreply.github.com>
2026-01-15 16:49:51 -0800

    Update create_files_with_subdirs.sh

--- create_files_with_subdirs.sh
+++ create_files_with_subdirs.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
-echo "$(date)"
-mkdir "$(date)"
+langs=("Assembly", "Autocode", "FORTRAN", "LISP", "COBOL", "ALGOL", "Smalltalk", "C", "SML", "Perl")
+twodigits=("01","02","03","04","05","06","07","08","09","10")
+now="$(date)"
+mkdir "$now"
+
+for value in "${twodigits[@]}" do
+	mkdir "${now}/file1${value}"
+done
